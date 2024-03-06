ac4596a1fec8e9240e16a61613dea1db558b3252
Alexandre <alevaristofig@gmail.com>
2024-03-05 23:18:27 -0300

    Adicionar o crossOrigin no controller alimento

--- SistemaControlePeso/src/main/java/com/sistemacontrolepeso/api/controller/AlimentoController.java
+++ SistemaControlePeso/src/main/java/com/sistemacontrolepeso/api/controller/AlimentoController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import com.sistemacontrolepeso.domain.model.Alimento;
 import com.sistemacontrolepeso.domain.repository.AlimentoRepositoy;
 import com.sistemacontrolepeso.domain.service.CadastroAlimentoService;
 
+@CrossOrigin(originPatterns = "*")
 @RestController
 @RequestMapping(value = "/alimentos", produces = MediaType.APPLICATION_JSON_VALUE)
 public class AlimentoController {
