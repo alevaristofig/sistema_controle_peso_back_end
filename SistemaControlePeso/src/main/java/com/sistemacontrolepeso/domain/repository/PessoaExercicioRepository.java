55344c515b895da49f88f1ae80820772b02e30d9
Alexandre <alevaristofig@gmail.com>
2024-04-15 16:58:53 -0300

    Criação do metodo para listar a quantidade de treinos da pessoa

--- SistemaControlePeso/src/main/java/com/sistemacontrolepeso/domain/repository/PessoaExercicioRepository.java
+++ SistemaControlePeso/src/main/java/com/sistemacontrolepeso/domain/repository/PessoaExercicioRepository.java
@@ -1,9 +1,21 @@
 package com.sistemacontrolepeso.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.sistemacontrolepeso.domain.model.PessoaExercicio;
 
+
 public interface PessoaExercicioRepository extends JpaRepository<PessoaExercicio, Long> {
 
+	@Query("select new com.sistemacontrolepeso.domain.model.dto.PessoaExercicio("
+			+ "pe.exercicio.id, count(pe), e.nome) from PessoaExercicio pe "
+			+ "inner join pe.exercicio e "
+			+ "where treino= :treino "
+			+ "group by  pe.exercicio")
+	public List<com.sistemacontrolepeso.domain.model.dto.PessoaExercicio> listarQuantidadeTreinos(
+			@Param("treino") char treino);
 }
